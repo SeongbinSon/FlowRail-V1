499719ff1a3ac80e2dcde9a1a8cfb78fc9c77b07
Seongbin Son <152048071+SeongbinSon@users.noreply.github.com>
2024-05-11 02:53:56 +0000

    API 연동 및 자료 불러오기

---
+++ FlowRail/API.py
@@ -0,0 +1,32 @@
+import requests
+
+# 실시간 역 도착정보 (RTSA) [강남역]
+# RTSA API KEY = 476a4267646572723737724355686d
+RTSA_url_Gangnam = "http://swopenAPI.seoul.go.kr/api/subway/476a4267646572723737724355686d/json/realtimeStationArrival/1/5/강남"
+
+# 열차 실시간 위치 (RTP) [2호선]
+# RTP API KEY = 795476586f6572723338674d467250
+RTP_url_line2 = "http://swopenAPI.seoul.go.kr/api/subway/795476586f6572723338674d467250/json/realtimePosition/1/5/2호선"
+
+
+
+
+RTSA_get_info = requests.get(RTSA_url_Gangnam) # 실시간 역 도착정보 불러오기
+RTSA_get_info = RTSA_get_info.json()
+print("강남역")
+print("==========================================")
+print("열차번호")
+print(RTSA_get_info['realtimeArrivalList'][1]['btrainNo'])
+print("열차정보 생성 시간")
+print(RTSA_get_info['realtimeArrivalList'][1]['recptnDt'])
+print("첫번째 도착메시지")
+print(RTSA_get_info['realtimeArrivalList'][1]['arvlMsg2']) # [RTSA] 첫번째 도착메시지
+print("두번째 도착메시지")
+print(RTSA_get_info['realtimeArrivalList'][1]['arvlMsg3']) # [RTSA] 두번째 도착메시지
+print("열차도착예정시간 (초)")
+print(RTSA_get_info['realtimeArrivalList'][1]['barvlDt']) # [RTSA] 열차도착예정시간
+print("==========================================")
+
+RTP_get_info = requests.get(RTP_url_line2)
+RTP_get_info = RTP_get_info.json()
+print(RTP_get_info['realtimePositionList'][1]['subwayNm'])
