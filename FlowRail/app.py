182d67ff631f177f6961ae4aa57bdaee93de0838
Seongbin Son <152048071+SeongbinSon@users.noreply.github.com>
2024-07-13 03:04:33 +0000

    테스트배드 /subway 부분, 호선 선택 및 근처 열차번호, 남은 시간, 열차 상태 표현 구현 1차 완료

--- FlowRail/app.py
+++ FlowRail/app.py
@@ -82,14 +82,67 @@ def getForm():
 #subway 기능 구현 테스트배드
 @app.route('/getsearch',methods=['POST', 'GET'])
 def getsubway():
-    name = request.form['stationName']
-    RTSA_url_First_search = "http://swopenAPI.seoul.go.kr/api/subway/476a4267646572723737724355686d/json/realtimeStationArrival/1/0/"+name
-    RTSA_get_info = requests.get(RTSA_url_First_search) # 실시간 역 도착정보 불러오기
-    RTSA_get_info = RTSA_get_info.json()
-    arrivaltime = RTSA_get_info['realtimeArrivalList'][0]['barvlDt']
 
+    #arrivaltime , infomation_test , arvlcode 지정
+    arrivaltime = 0
+    infomation_test = 1
+    arvlcode = "default"
+
+    #이름 및 호선 지정
+    name = request.form['stationName']
+    line = request.form['line']
+
+    #터미널 확인 위한 출력문
+    print(line)
+   
+   #열차 검색을 위한 RTSA_url_First_search
+    RTSA_url_First_search = "http://swopenAPI.seoul.go.kr/api/subway/476a4267646572723737724355686d/json/realtimeStationArrival/0/40/"+name
     
-    return render_template('search.html', time = arrivaltime)
+    # 실시간 역 도착정보 불러오기
+    RTSA_get_info = requests.get(RTSA_url_First_search)
+
+    # 정보 json 변환
+    RTSA_get_info = RTSA_get_info.json()
+
+    # 2호선 근처 열차 검색 부분
+    for i in range(len(RTSA_get_info['realtimeArrivalList'])):
+        if RTSA_get_info['realtimeArrivalList'][i]['subwayId'] == "1002":
+            arrivaltime = RTSA_get_info['realtimeArrivalList'][i]['barvlDt']
+            infomation_test = RTSA_get_info['realtimeArrivalList'][i]['btrainNo']
+
+            #arvlcode 한글변환 부분
+            if RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "0":
+                arvlcode = "진입"
+
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "1":
+                arvlcode = "도착"
+            
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "2":
+                arvlcode = "출발"
+
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "3":
+                arvlcode = "전역츨발"
+
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "4":
+                arvlcode = "전역진입"
+            
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "5":
+                arvlcode = "전역도착"
+
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "6":
+                arvlcode = "이거뭐야"
+
+            elif RTSA_get_info['realtimeArrivalList'][i]['arvlCd'] == "99":
+                arvlcode = "운행중"
+            break
+
+    #터미널 확인 위한 출력물    
+    print(arrivaltime)
+    print(arvlcode)
+    print(infomation_test)
+    return render_template('search.html', time = arrivaltime , arrivalcode = arvlcode , train_number = infomation_test)
+
+# /* ------------------------------------------------------------------------------------------------ */
 
 if __name__ == '__main__':
     app.run()
